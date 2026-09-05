e1d6b75e4544ca5c9ccbb477496e3867cd3efc5e
manik rao <manikrao943@gmail.com>
2026-09-05 22:34:25 +0530

    Add number checker program

---
+++ number_checker.py
@@ -0,0 +1,15 @@
+# Number Checker
+
+number = 25
+
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
